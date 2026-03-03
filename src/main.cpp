b96041797de2081cfb29e96a8bd66c4be50dfeee
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-03-03 14:01:08 -0800

    removed BGFX

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <format>
 
+#include <miniaudio.h>
+#include <entt/entt.hpp>
+#include <Jolt/Jolt.h>
+
 int main() {
     std::string s = std::format("Hello, {}!", "world");
     std::cout << s << std::endl;
