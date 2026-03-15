72f9eae9c2fad4b9b4a206fd07cac4a748452e8e
metinis <denis.muraska@gmail.com>
2026-03-15 13:04:30 -0400

    fix some shader and copying asset stuff

--- src/renderer/backends/vulkan/Core.cpp
+++ src/renderer/backends/vulkan/Core.cpp
@@ -36,6 +36,7 @@ void SYN::VulkanBackend::init(Window &window) {
     VkShaderModule vertShaderModule{
         createShaderModule(device, "generated/shaders/first.vert.spv")};
 
+
     VkPipelineShaderStageCreateInfo vertPipeCI{
         .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
         .stage = VK_SHADER_STAGE_VERTEX_BIT,
