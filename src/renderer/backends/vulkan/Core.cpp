3072cb73272744aabd9c08ba304d647951106087
John Fleming <flemingjohnrobert.jrf@gmail.com>
2026-03-15 02:48:41 -0400

    fixed shader compilation and cleaned up resource dir copy, got shader module creation working

--- src/renderer/backends/vulkan/Core.cpp
+++ src/renderer/backends/vulkan/Core.cpp
@@ -4,11 +4,18 @@
 #include "Swapchain.h"
 
 #include <GLFW/glfw3.h>
+#include <filesystem>
+#include <fstream>
 #include <spdlog/spdlog.h>
 
 #include <PuzzleEngine/core/Window.h>
 
 using namespace SYN;
+
+namespace {
+VkShaderModule createShaderModule(const Device &device,
+                                  const std::string &path);
+}
 
 void SYN::VulkanBackend::init(Window &window) {
     VkInstance instance{createInstance()};
@@ -26,8 +33,21 @@ void SYN::VulkanBackend::init(Window &window) {
     Device device{createDevice(instance, surface)};
     Swapchain swapchain{createSwapchain(device, surface, window)};
 
+    VkShaderModule vertShaderModule{
+        createShaderModule(device, "generated/shaders/first.vert.spv")};
+
+    VkPipelineShaderStageCreateInfo vertPipeCI{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage = VK_SHADER_STAGE_VERTEX_BIT,
+        .module = vertShaderModule,
+        .pName = "main",
+    };
+
+    vkDestroyShaderModule(device.logical, vertShaderModule, nullptr);
+
     m_State = VulkanState{.instance = instance,
                           .surface = surface,
+                          .debugUtilsMessenger = debugUtilsMessenger,
                           .device = device,
                           .swapchain = swapchain};
 }
@@ -40,3 +60,52 @@ void SYN::VulkanBackend::shutdown() {
 
     m_State = {};
 }
+
+// TODO: add default shaders for if theres an error
+namespace {
+VkShaderModule createShaderModule(const Device &device,
+                                  const std::string &path) {
+    std::ifstream file(path, std::ios::in | std::ios::binary | std::ios::ate);
+
+    if (!file.is_open()) {
+        spdlog::error("Could not open {}, shader module creation failed", path);
+        assert(false);
+        return {};
+    }
+    std::streampos fileSize{file.tellg()};
+    if (fileSize < 0) {
+        spdlog::error("Tellg failed, shader module creation failed", path);
+        assert(false);
+        return {};
+    }
+
+    if ((fileSize % sizeof(uint32_t)) != 0) {
+        spdlog::error("Could not create shader module, {} file size was not a "
+                      "multiple of 32 "
+                      "and may be malformed",
+                      path);
+        assert(false);
+        return {};
+    }
+
+    std::vector<uint32_t> shaderCode(fileSize / sizeof(uint32_t));
+    file.seekg(0, std::ios::beg);
+    file.read(reinterpret_cast<char *>(shaderCode.data()), fileSize);
+
+    VkShaderModuleCreateInfo shaderModuleCI{
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .codeSize = shaderCode.size() * sizeof(uint32_t), // byte size
+        .pCode = shaderCode.data()};
+
+    VkShaderModule shaderModule{};
+    VkResult res{vkCreateShaderModule(device.logical, &shaderModuleCI, nullptr,
+                                      &shaderModule)};
+    if (res != VK_SUCCESS) {
+        spdlog::error("Could not create shader module of {},  VkResult = {}",
+                      path, static_cast<int>(res));
+        assert(false);
+        return {};
+    }
+    return shaderModule;
+}
+} // namespace
