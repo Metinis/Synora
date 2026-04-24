08f4dd58bce8857da46dfa7f1808dc39c6f27b9b
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-04-24 03:47:18 -0700

    texture example

--- examples/gl_test.cpp
+++ examples/gl_test.cpp
@@ -2,6 +2,9 @@
 
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
@@ -23,12 +26,15 @@ int main(void) {
         #version 450 core
         layout (location = 0) in vec3 aPos;
         layout (location = 1) in vec3 aColor;
+        layout (location = 2) in vec2 aTexCoords;
 
         out vec4 vertexColor;
+        out vec2 fragTexCoords;
 
         uniform float scale;
 
         void main() {
+            fragTexCoords = aTexCoords;
             vertexColor = vec4(aColor, 1.0);
             gl_Position = vec4(aPos * scale, 1.0);
         }
@@ -39,15 +45,20 @@ int main(void) {
         out vec4 fragColor;
 
         in vec4 vertexColor;
+        in vec2 fragTexCoords;
+
+        uniform sampler2D texture0;
 
         void main() {
-            fragColor = vertexColor;
+            vec4 imageTex = texture(texture0, fragTexCoords);
+            fragColor = mix(imageTex, vertexColor, 0.0);
         }
     )";
 
-    float vertices[] = {0.5, 0.5, 0.0,  1.0, 0.0, 0.0,  -0.5, -0.5,
-                        0.0, 0.0, 1.0,  0.0, 0.5, -0.5, 0.0,  0.0,
-                        0.0, 1.0, -0.5, 0.5, 0.0, 1.0,  1.0,  0.0};
+    float vertices[] = {0.5,  0.5,  0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+                        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                        0.5,  -0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+                        -0.5, 0.5,  0.0, 1.0, 1.0, 0.0, 0.0, 1.0};
 
     gl::Handle<gl::Shader> mainShader =
         glContext.createShader(vertexSource, fragmentSource).value();
@@ -71,17 +82,36 @@ int main(void) {
     gl::VertexArrayDesc basicVertexDesc;
     basicVertexDesc.indexBufferHandle = indexBuffer;
     basicVertexDesc.vertexBufferHandle = vertexBuffer;
-    basicVertexDesc.vertexBufferStride = sizeof(float) * 6;
+    basicVertexDesc.vertexBufferStride = sizeof(float) * 8;
     basicVertexDesc.attributes[0] = {0, gl::VertexFormat::Float3, 0, false};
     basicVertexDesc.attributes[1] = {1, gl::VertexFormat::Float3,
                                      sizeof(float) * 3, false};
-    basicVertexDesc.attributeCount = 2;
+    basicVertexDesc.attributes[2] = {2, gl::VertexFormat::Float2,
+                                     sizeof(float) * 6, false};
+    basicVertexDesc.attributeCount = 3;
 
     gl::Handle<gl::VertexArray> vertexArray =
         glContext.createVertexArray(basicVertexDesc).value();
 
     gl::PipelineState defaultPipeline;
     defaultPipeline.shader = mainShader;
+
+    stbi_set_flip_vertically_on_load(true);
+
+    int x, y, nrC;
+    uint8_t *image_data =
+        stbi_load("resources/textures/talkischeap.jpg", &x, &y, &nrC, 0);
+
+    gl::TextureDesc textureDesc;
+    textureDesc.width = x;
+    textureDesc.height = y;
+    textureDesc.format = gl::TextureFormat::RGB8;
+
+    gl::Handle<gl::Texture> texture =
+        glContext.createTexture(textureDesc, image_data).value();
+    gl::Handle<gl::Sampler> sampler = glContext.createSampler({}).value();
+
+    stbi_image_free(image_data);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -99,7 +129,10 @@ int main(void) {
 
             float scale = std::sin(glfwGetTime());
 
+            pass.bindTexture(0, texture, sampler);
+
             pass.bindUniform("scale", scale);
+            pass.bindUniform("texture0", 0);
 
             pass.bindVertexArray(vertexArray);
             pass.drawIndexed(6);
