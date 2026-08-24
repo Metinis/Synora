d1d187a11dc97e740748d053f368f5ebf849c223
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-08-23 17:20:03 -0700

    update transform component default syntax

--- SynoraEngine/include/SynoraEngine/scene/components/TransformComponent.h
+++ SynoraEngine/include/SynoraEngine/scene/components/TransformComponent.h
@@ -5,9 +5,9 @@
 
 namespace SYN {
 struct TransformComponent {
-    glm::vec3 position{0.f, 0.f, 0.f};
+    glm::vec3 position = glm::vec3(0.0f);
     glm::quat rotation = glm::quat(1.f, 0.f, 0.f, 0.f);
-    glm::vec3 scale{1.f, 1.f, 1.f};
+    glm::vec3 scale = glm::vec3(1.0f);
 
     glm::mat4 getLocalMatrix() const;
     void setLocalMatrix(glm::mat4 m);
