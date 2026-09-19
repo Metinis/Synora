8d6ac9a83fcefd53d5e58ef3e05d9da292acc748
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-09-19 02:27:42 -0700

    default sample attachment slot

--- SynoraEngine/include/SynoraEngine/project/assets/RenderTargetData.h
+++ SynoraEngine/include/SynoraEngine/project/assets/RenderTargetData.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SynoraEngine/renderer/types/InputSlot.h>
+
 namespace SYN {
 struct RenderTargetData {
     enum class Format {
@@ -24,5 +26,8 @@ struct RenderTargetData {
     uint32_t width = 0;
     uint32_t height = 0;
     uint32_t sampleCount = 1;
+
+    InputSlot::RenderTargetInput::AttachmentType defaultSampleAttachment =
+        InputSlot::RenderTargetInput::AttachmentType::Color0;
 };
 } // namespace SYN
